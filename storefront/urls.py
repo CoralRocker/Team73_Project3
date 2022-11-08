cef6b1dfcb7a7d83c05c87c2ad65742dcea94964
haydenf7f <hayden7mf@gmail.com>
2022-11-08 15:18:20 -0600

    Added menu pages for all categories

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
 from .views import HomePageView, MenuPageView
+from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('menu/', MenuPageView.as_view(), name='menu')   
+    path('menu/', MenuPageView.as_view(), name='menu'),  
+    path('menu/espresso/', views.EspressoPageView, name="espresso"),
+    path('menu/brewed/', views.BrewedPageView, name="brewed"),
+    path('menu/blended/', views.BlendedPageView, name="blended"),
+    path('menu/tea/', views.TeaPageView, name="tea"),
+    path('menu/other/', views.OtherPageView, name="other"),
 ]
