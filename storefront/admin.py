9c1861269fd7bbdd13deb1a39ce514365b4341a2
haydenf7f <hayden7mf@gmail.com>
2022-11-08 21:30:04 -0600

    Added admin search options for Menu

--- storefront/admin.py
+++ storefront/admin.py
@@ -10,12 +10,15 @@ from .models import (
     Ingredient
 )
 
+class MenuAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name__icontains', 'size', 'price', 'type']
+
 # Register your models here.
 admin.site.register(Customization)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Finance)
 admin.site.register(Inventory)
-admin.site.register(Menu)
+admin.site.register(Menu, MenuAdmin)
 admin.site.register(Ingredient)
 
