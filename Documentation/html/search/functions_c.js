316c7d25e33a8352fb143264551fe2ca587e5d3d
Howiemorg <morgenthalerhowie@yahoo.com>
2022-12-06 19:06:34 -0600

    added doxygen doc

---
+++ Documentation/html/search/functions_c.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['salespageview_196',['SalesPageView',['../namespacestorefront_1_1views.html#afd9f7f20bad22455da23840a9fd9c897',1,'storefront::views']]],
+  ['searchpageview_197',['SearchPageView',['../namespacestorefront_1_1views.html#a578ae9d408fa2493fe6a466110784549',1,'storefront::views']]],
+  ['setsizes_198',['setSizes',['../classstorefront_1_1forms_1_1CustomizationForm.html#a513a87aed7c5c3515770ad3684f04cc9',1,'storefront::forms::CustomizationForm']]]
+];
