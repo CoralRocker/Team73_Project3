af8296403c384d3c5cfef5d5bd9791193d1b0684
Gaultier Delbarre <gaultiertexas@gmail.com>
2022-12-12 09:17:09 -0600

    small changes

--- storefront/admin.py
+++ storefront/admin.py
@@ -9,9 +9,21 @@ from .models import (
     Menu,
     Ingredient
 )
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
 
 class MenuAdmin(admin.ModelAdmin):
     search_fields = ['id', 'name__icontains', 'size__iexact', 'price', 'type__iexact']
+    inlines = (IngredientInline,)
+
+class InventoryAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name', 'stock', 'amount_per_unit', 'price']
+
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'menu_item__name', 'inventory__name']
+    fields = ['menu_item', 'inventory', 'amount']
+    readonly_fields = tuple()
 
 class CustomizationAdmin(admin.ModelAdmin):
     search_fields = ['id', 'name__icontains', 'type__iexact']
@@ -21,7 +33,7 @@ admin.site.register(Customization, CustomizationAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Finance)
-admin.site.register(Inventory)
+admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Menu, MenuAdmin)
-admin.site.register(Ingredient)
+admin.site.register(Ingredient, IngredientAdmin)
 
